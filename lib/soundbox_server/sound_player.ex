6e7ed9a3b69b9178d803bcac6570f74bdba8659f
Alexander Marold <alexander.marold@invision.de>
2017-11-07 20:30:00 +0100

    Add rudimentary player

---
+++ lib/soundbox_server/sound_player.ex
@@ -0,0 +1,22 @@
+defmodule SoundboxServer.SoundPlayer do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, [])
+  end
+
+  def init(nil) do
+    port = Port.open({:spawn, "mpg123 -"}, [:binary])
+    {:ok, port}
+  end
+
+  def play(pid, {port, key}) do
+    GenServer.cast(pid, {:play, key})
+  end
+
+  def handle_cast({:play, key}, port) do
+    [{_, file}] = :ets.lookup(:mp3, key)
+    Port.command(port, file)
+    {:noreply, port}
+  end
+end
