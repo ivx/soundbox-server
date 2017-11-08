05fd3a16ec5002e2ad25d5dd8402d280535811e3
Alexander Marold <alexander.marold@invision.de>
2017-11-08 11:47:17 +0100

    Autorecovery for SoundPlayer

--- lib/soundbox_server/sound_player.ex
+++ lib/soundbox_server/sound_player.ex
@@ -6,7 +6,9 @@ defmodule SoundboxServer.SoundPlayer do
   end
 
   def init(nil) do
-    port = Port.open({:spawn, "mpg123 -"}, [:binary])
+    port = Port.open({:spawn, device()}, [:binary])
+    Process.send_after(self(), :check_port_alive, 1000)
+
     {:ok, port}
   end
 
@@ -18,5 +20,18 @@ defmodule SoundboxServer.SoundPlayer do
     [{_, _, file}] = :dets.lookup(:mp3, key)
     Port.command(port, file)
     {:noreply, port}
+  end
+
+  def handle_info(:check_port_alive, port) do
+    new_port = if Port.info(port),
+      do: port,
+      else: Port.open({:spawn, device()}, [:binary])
+
+    Process.send_after(self(), :check_port_alive, 1000)
+    {:noreply, new_port}
+  end
+
+  defp device do
+    Application.get_env(:soundbox_server, :audio_player, "mpg123 -")
   end
 end
