e1b2224394847545e26c38dd5c4e7a25b7288b80
Mario Mainz <mario.mainz@invision.de>
2017-11-08 13:14:12 +0100

    Add sounds channel to user socket

--- lib/soundbox_server_web/channels/user_socket.ex
+++ lib/soundbox_server_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule SoundboxServerWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", SoundboxServerWeb.RoomChannel
+  channel "sound:*", SoundboxServerWeb.SoundChannel
 
   ## Transports
   transport(:websocket, Phoenix.Transports.WebSocket)
